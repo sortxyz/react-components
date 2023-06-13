4c83a2fe20fc773c2235846ba8e250f1239e58f1
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-12 18:08:21 -0700

    chore: allow named exports to be imported with object destructuring syntax

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,25 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
-
-const packageJson = require("./package.json");
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import packageJson from './package.json';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
-      sourcemap: true
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
-      sourcemap: true
-    }
+      format: 'esm',
+      exports: 'named',
+      sourcemap: true,
+    },
   ],
   plugins: [
     peerDepsExternal(),
@@ -26,7 +27,7 @@ export default {
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
-        extensions: ['.css']
-    })
-  ]
+      extensions: ['.css'],
+    }),
+  ],
 };
