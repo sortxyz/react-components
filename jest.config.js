721bcdc54e8b18c5d377628cb627954278619caf
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-13 23:29:51 -0700

    chore: enhance test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@styles/(.*)$': '<rootDir>/src/components/styles/$1',
     '^@globalStyles/(.*)$': '<rootDir>/src/components/styles/$1',
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/__mocks__/styleMock.ts',
   },
+  setupFiles: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jsdom',
 };
