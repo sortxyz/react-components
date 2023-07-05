12716ca13d5c448252249497db806989af4df5e0
Aaron Reisman <aaron@lifeiscontent.net>
2023-07-05 14:37:17 -0700

    chore: fix asset bundling

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,19 @@
-const path = require('path');
+const { ESLint } = require('eslint')
 
-const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    })
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-};
+  '**/*.{ts,tsx,js,jsx}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`eslint --fix --max-warnings=0 ${filesToLint}`]
+  },
+}
